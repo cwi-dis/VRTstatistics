b90bf30e98430d87bf46905d561cff9d30cf2dbe
Jack Jansen <Jack.Jansen@cwi.nl>
2021-12-07 17:11:55 +0100

    Get sender logs as well

--- scripts/getlogs-jitter.sh
+++ scripts/getlogs-jitter.sh
@@ -9,6 +9,7 @@ set -e
 #SENDER_SSH_ID=vr-together@vrsmall.local
 #RECEIVER_SSH_ID=vrtogether@scallion.local
 # Jack home
+SENDER_SSH_ID=vrtogether@vrtiny.local
 RECEIVER_SSH_ID=localhost
 
 # Find script directory and python
@@ -23,16 +24,18 @@ winPath="AppData/LocalLow/i2Cat/VRTogether/statistics.log"
 macPath="Library/Application\ Support/i2Cat/VRTogether/statistics.log"
 set -x
 # Get datafiles
+scp "${SENDER_SSH_ID}:$winPath" sender.log
 scp "${RECEIVER_SSH_ID}:$macPath" receiver.log
 # convert to json
+python $scriptdir/stats2json.py sender.log sender.json
 python $scriptdir/stats2json.py receiver.log receiver.json
 # There's nothing to conbine, but we do want to fix timestamps and all that
-python $scriptdir/combine.py - receiver.json combined.json
+python $scriptdir/combine.py sender.json receiver.json combined.json
 # Show a graph with rendered pointcloud sizes
 # This is expected to be "good enough" to judge whether we're doing the right thing.
-python $scriptdir/filter.py combined.json pointcloud_sizes.csv '"PointCloudRenderer" in component' sessiontime component=points_per_cloud
+python $scriptdir/filter.py combined.json pointcloud_sizes.csv 'role == "receiver" and "PointCloudRenderer" in component' sessiontime component=points_per_cloud
 python $scriptdir/plot.py pointcloud_sizes.csv
 #
 # Graph latencies
-python $scriptdir/filter.py combined.json pointcloud_latencies.csv '"PointCloudRenderer" in component' sessiontime component=latency_ms
+python $scriptdir/filter.py combined.json pointcloud_latencies.csv 'role == "receiver" and "PointCloudRenderer" in component' sessiontime component=latency_ms
 python $scriptdir/plot.py pointcloud_latencies.csv
