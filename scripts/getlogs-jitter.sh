7fd7b03f650f3a2ceeb3d821c76e0406a45c29fd
Jack Jansen <Jack.Jansen@cwi.nl>
2021-12-08 00:40:14 +0100

    Show queue durations in latency graph, show timestamp graph

--- scripts/getlogs-jitter.sh
+++ scripts/getlogs-jitter.sh
@@ -33,9 +33,13 @@ python $scriptdir/stats2json.py receiver.log receiver.json
 python $scriptdir/combine.py sender.json receiver.json combined.json
 # Show a graph with rendered pointcloud sizes
 # This is expected to be "good enough" to judge whether we're doing the right thing.
-python $scriptdir/filter.py combined.json pointcloud_sizes.csv 'role == "receiver" and "PointCloudRenderer" in component' sessiontime component=points_per_cloud
+python $scriptdir/filter.py combined.json pointcloud_sizes.csv 'role == "receiver" and "PointCloudRenderer" in component' sessiontime points_per_cloud
 python $scriptdir/plot.py pointcloud_sizes.csv
 #
 # Graph latencies
-python $scriptdir/filter.py combined.json pointcloud_latencies.csv 'role == "receiver" and "PointCloudRenderer" in component' sessiontime component=latency_ms
+python $scriptdir/filter.py combined.json pointcloud_latencies.csv '("renderer_queue_ms" in record and role == "receiver") or "decoder_queue_ms" in record or "encoder_queue_ms" in record or "transmitter_queue_ms" in record' sessiontime latency_ms renderer_queue_ms decoder_queue_ms transmitter_queue_ms encoder_queue_ms
 python $scriptdir/plot.py pointcloud_latencies.csv
+#
+# Graph timestamps
+python $scriptdir/filter.py combined.json pointcloud_timestamps.csv '"timestamp" in record and timestamp > 0' sessiontime role.component=timestamp
+python $scriptdir/plot.py pointcloud_timestamps.csv
