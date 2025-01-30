90cf0477097638de89285b0b5621141ece6dd505
Jack Jansen <Jack.Jansen@cwi.nl>
2025-01-30 16:13:24 +0100

    Removed dead wood.

--- VRTrun/src/VRTrun/__main__.py
+++ VRTrun/src/VRTrun/__main__.py
@@ -48,35 +48,5 @@ def main():
 
     return sts
 
-       
-    sender_stats = os.path.join(destdir, "sender.log")
-    receiver_stats = os.path.join(destdir, "receiver.log")
-    combined = os.path.join(destdir, "combined.json")
-    sender_logfile = os.path.join(destdir, "sender-unity-log.txt")
-    receiver_logfile = os.path.join(destdir, "receiver-unity-log.txt")
-   
-    if not args.nofetch:
-        assert sender
-        assert receiver
-        if not args.nolog:
-            sender.get_log(sender_logfile)
-            receiver.get_log(receiver_logfile)
-        
-        sender.get_stats(sender_stats)
-        receiver.get_stats(receiver_stats)
-        if not args.norusage:
-
-            sender_rusagefile = os.path.join(destdir, "sender-rusage.log")
-            receiver_rusagefile = os.path.join(destdir, "receiver-rusage.log")
-
-            sender.get_remotefile("rusage.log", sender_rusagefile)
-            receiver.get_remotefile("rusage.log", receiver_rusagefile)
-            #
-            # Bit of a hack: we append resource usage to stats, so ts mapping is done correctly.
-            #
-            open(sender_stats, 'a').write(open(sender_rusagefile).read())
-            open(receiver_stats, 'a').write(open(receiver_rusagefile).read())
-    return 0
-
 if __name__ == "__main__":
     main()
